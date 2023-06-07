61a295e303d18804ac0cf0e7340fe8aa6dc900f9
Umut <umtakpnr5@gmail.com>
2023-06-08 00:48:24 +0300

    Ben de değiştirdim title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter f21 Demo',
+      title: 'Flutter f21 Demo 3',
       theme: ThemeData(
         // This is the theme of your application.
         //

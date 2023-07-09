c13a999b89afa0af20dda8348606b6e6dc1baeb4
Umut <umtakpnr5@gmail.com>
2023-07-09 20:50:14 +0300

    Kullanıcı bilgileri artık update edilebilir.

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:f21_demo/core/providers/firebase_providers.dart';
 import 'package:f21_demo/core/utils.dart';
 import 'package:f21_demo/features/auth/controller/auth_controller.dart';
@@ -27,17 +30,74 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   bool _isSaved = true;
 
   //userInfoStates
-  //File? profileFile;
-  DateTime? birthDate;
-  DateTime? birthDateBaby;
-  double? months;
-  String gender = "Belirsiz";
-  late bool isPregnant = ref.read(userProvider)!.isPregnant!;
+  String _localProfilePicturePath = ""; //firebaseden indirilip locale kaydedilen localProfilePicturePath
+  File? _profilePicFile; //güncelleneceği zaman localden upload edilen kontrol et null ise _profilePic değerini indirip al
+  String? _profilePic; //firebase'den gelen url
+  DateTime? _birthDate;
+  DateTime? _birthDateBaby;
+  double? _months;
+  String? _gender = "Belirsiz";
+  late bool _isPregnant;
+
+  void selectProfileImage() async {
+    final res = await pickImage();
+    if (res != null) {
+      setState(() {
+        _profilePicFile = File(res.files.first.path!);
+      });
+    }
+  }
+
+  downloadExistingProfileImageFromFirebaseAsFile(String imageUrl) async {
+    print("RECO : $imageUrl");
+    var response = await http.get(Uri.parse(imageUrl));
+    if (response.statusCode == 200) {
+      var contentType = response.headers['content-type'];
+      if(contentType != null){
+        var fileExtension = getFileExtensionFromContentType(contentType);
+        //print("RECO : file extension: $fileExtension");
+        var fileName = 'existing_pofile_pic$fileExtension';
+        //print("RECO : file name: $fileName");
+        var appDocDir = await getApplicationDocumentsDirectory();
+        //print("RECO : application documents dir: $appDocDir");
+        var filePath = '${appDocDir.path}/$fileName';
+        //print("RECO : file extension: $filePath");
+        var file = File(filePath);
+        await file.writeAsBytes(response.bodyBytes); //Bu kısmı bir yere eşitlemek lazım mı? sanki boş mu kaldı
+
+        setState(() {
+          _localProfilePicturePath = filePath;
+          _profilePicFile = file;
+        });
+
+        print("RECO : _profilePicFile: $_profilePicFile");
+        //print('Görsel kaydedildi: ${file.path}');
+      }
+    } else {
+      print('Var olan profil resmi indirilirken bir sorun oluştu : ${response.statusCode}');
+    }
+  }
+
+  String getFileExtensionFromContentType(String contentType) {
+    if (contentType == 'image/jpeg') {
+      return '.jpg';
+    } else if (contentType == 'image/png') {
+      return '.png';
+    } else if (contentType == 'image/gif') {
+      return '.gif';
+    }
+    // Diğer dosya türleri için ilgili uzantıları burada belirtebilirsiniz.
+    // Örneğin: 'image/svg+xml' -> '.svg'
+
+    // Varsayılan olarak, bilinmeyen dosya türleri için bir uzantı döndürebilirsiniz.
+    return '.unknown';
+  }
 
   //controllers
   final _usernameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool oneTimeWorkController = false;
 
   //functions
   void _toggleSwitch() {
@@ -51,10 +111,17 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 
   void _setInitialValuesOfFormFields(UserModel? user, FirebaseAuth userAuth) {
     setState(() {
-      _usernameController.text = user!.username!.toString();
+      //Profil resmi değerini de burada almak lazım set ettikten sonra ekranda gösterirken de bu set ettiğin değişkeni kullanarak göster
+      _profilePic = user!.profilePic!;
+      _usernameController.text = user.username!.toString();
       _emailController.text = userAuth.currentUser!.email!;
       _passwordController.text = "*****";
+      _months = user.months;
+      _birthDate = user.birthDate;
+      _birthDateBaby = user.babyBirthDate;
+      _isPregnant = ref.read(userProvider)!.isPregnant!;//bu metodun retun değeri yok direkt olarak yukarıdaki _profilePicFile'a eşitler
     });
+    downloadExistingProfileImageFromFirebaseAsFile(_profilePic!);
     //TODO: Ben değerleri aşağıdaki fromda bulunan değerleri elle atadım ancak _formKey kullanılarak ilgili alanların initialValue'leri bu fonksiyonda atanabilir.
   }
 
@@ -62,6 +129,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   @override
   void initState() {
     // TODO: implement initState
+    oneTimeWorkController = true;
     super.initState();
   }
 
@@ -77,11 +145,14 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   Widget build(BuildContext context) {
     bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
     CustomStyles().responsiveTheme(isDarkMode);
-    //providers
-    final UserModel? user = ref.watch(userProvider);
-    final FirebaseAuth userAuth = ref.watch(authProvider);
+  //providers
+    final UserModel? user = ref.read(userProvider);
+    final FirebaseAuth userAuth = ref.read(authProvider);
 
-    _setInitialValuesOfFormFields(user, userAuth);
+    if(oneTimeWorkController){
+      _setInitialValuesOfFormFields(user, userAuth);
+      oneTimeWorkController = false;
+    }
     return WillPopScope(
       onWillPop: () {
         if (_isSaved) {
@@ -130,12 +201,21 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                         //TODO: Profil Resmi
                         InkWell(
                           onLongPress: () {
-                              showSnackBar(context, "Profil resminizi değiştirmek için Bilgilerimi Düzenle'yi aktifleştirin ve bu alana tıklayın ...");
+                              String textMessage = "Profil resminizi değiştirmek için Bilgilerimi Düzenle'yi aktifleştirin ve bu alana tıklayın ...";
+                              if (_isEditing) {
+                                textMessage = "Profil resminizi değiştirmek için görsele bir kez tıklayın.";
+                              }
+                              showSnackBar(context, textMessage);
                             },
                           onTap: () {
                             //TODO: Görsel seçme işlemi
                             if (_isEditing) {
-                              showSnackBar(context, "Görsel seçme fonksiyonu çalışmalı");
+                              selectProfileImage(); //seçilen profil resmi ana ekranda gösterilmeli (bir bool değişken koyarsın en tepeye o değiştiğinde localden image göster çalışır aksi halde url'den image göster çalışır)
+                              if(_profilePicFile != null){ //görsel seçildiyse
+                                setState(() {
+                                  _localProfilePicturePath = _profilePicFile!.path; //yeni seçilen görselin pathini buraya kaydet
+                                });
+                              }
                             } else {
                               showSnackBar(context, "Öncelikile bilgilerimi düzenleyi aktifleştirin!");
                             }
@@ -146,8 +226,10 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                                 CircleAvatar(
                                   radius: 64,
                                   //TODO: Buraya kullanıcı resmi gelecek firebase ile çekilen resim
-                                  backgroundImage:
-                                      NetworkImage(user!.profilePic!),
+                                  backgroundImage: _localProfilePicturePath.isEmpty ? NetworkImage(_profilePic!) : FileImage(File(_localProfilePicturePath!),scale: 2) as ImageProvider,
+                                  //_localProfilePicturePath.isEmpty ? NetworkImage(_profilePic!) : FileImage(File(_localProfilePicturePath!),scale: 2), //localdeki pathi
+                                  //backgroundImage : NetworkImage(_profilePic!), //artık networkten çekmiyoruz
+
                                 ),
                                 const Positioned(
                                   bottom: 0,
@@ -220,7 +302,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                           validator: (value) {
                             if (value == null) {
                               return "Doğum tarihi boş bırakılamaz";
-                            } else if (((DateTime.now().year - value!.year) <
+                            } else if (((DateTime.now().year - value.year) <
                                 18)) {
                               return "18 yaşından küçükler bu uygulamayı kullanamaz!";
                             }
@@ -228,11 +310,11 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                           },
                           name: "birthDate",
                           lastDate: DateTime(DateTime.now().year - 18),
-                          initialDate: user!.birthDate ?? DateTime.now(),
-                          initialValue: user!.birthDate ?? DateTime.now(),
+                          initialDate: user!.birthDate,
+                          initialValue: user.birthDate,
                           onChanged: (value) {
                             setState(() {
-                              birthDate = value;
+                              _birthDate = value;
                             });
                           },
                           inputType: InputType.date,
@@ -245,18 +327,18 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                         FormBuilderSwitch(
                           enabled: _isEditing,
                           name: "role",
-                          subtitle: Text(isPregnant
+                          subtitle: Text(_isPregnant
                               ? "Henüz doğum yapmadım."
                               : "Doğum yaptım."),
                           title: Text(
-                            isPregnant ? "Gebeyim" : "Anneyim",
+                            _isPregnant ? "Gebeyim" : "Anneyim",
                             style: TextStyle(
                                 color: CustomStyles.titleColor, fontSize: 18),
                           ),
-                          initialValue: isPregnant,
+                          initialValue: _isPregnant,
                           onChanged: (value) {
                             setState(() {
-                              isPregnant = value!;
+                              _isPregnant = value!;
                             });
                           },
                           controlAffinity: ListTileControlAffinity.leading,
@@ -265,7 +347,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                               const InputDecoration(border: InputBorder.none),
                         ),
                         const SizedBox(height: 20),
-                        isPregnant
+                        _isPregnant
                             ? Column(children: [
                                 Align(
                                   alignment: Alignment.centerLeft,
@@ -285,11 +367,11 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                                   name: "months",
                                   min: 1,
                                   max: 9,
-                                  initialValue: user.months?.toDouble() ?? 2,
+                                  initialValue: _months ?? 2,
                                   divisions: 8,
                                   displayValues: DisplayValues.current,
-                                  onChangeEnd: (value) {
-                                    months = value;
+                                  onChanged: (value) {
+                                    _months = value;
                                   },
                                   valueWidget: (value) {
                                     return Container(
@@ -314,7 +396,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                                 //TODO: Bebeğin doğum tarihi ayarla
                                 FormBuilderDateTimePicker(
                                   validator: (value) {
-                                    if (value == null && !isPregnant) {
+                                    if (value == null && !_isPregnant) {
                                       return "Doğum tarihi boş bırakılamaz";
                                     }
                                     return null;
@@ -325,7 +407,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                                       user.babyBirthDate ?? DateTime.now(),
                                   name: "birthDate",
                                   onChanged: (value) {
-                                    birthDateBaby = value;
+                                    _birthDateBaby = value;
                                   },
                                   inputType: InputType.date,
                                   decoration: const InputDecoration(
@@ -358,7 +440,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                           initialValue: user.gender ?? "Belirsiz",
                           iconEnabledColor: CustomStyles.titleColor,
                           onChanged: (value) {
-                            gender = value!;
+                            _gender = value!;
                           },
                           iconDisabledColor: CustomStyles.titleColor,
                           style: TextStyle(
@@ -383,6 +465,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                                             Colors.red),
                                   ),
                                   onPressed: () {
+                                    showSnackBar(context, "İptal edildi");
                                     //TODO: Buna basıldığında tüm _formKey ile eski haline getirilmeli ya da pop yapıp yeniden bu sayfa açılabilir??? Şimdilik ikinciyi yapıyor
                                     setState(() {
                                       _isSaved = true;
@@ -398,11 +481,28 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                                             Colors.green),
                                   ),
                                   onPressed: () {
-                                    if (_formKey.currentState!.validate()) {
-                                      //TODO: Kaydetme işlemleri burada yapılacak eğer kaydet butonuna basılmadan sayfadan çıkılmak istenirse hata vermeli!
-                                      setState(() {
-                                        _isSaved = true;
-                                      });
+                                    try{
+                                      if (_formKey.currentState!.validate()) {
+                                        ref.read(authControllerProvider.notifier).setProfileInfos(
+                                            _usernameController.text,
+                                            _birthDate!,
+                                            _gender!,
+                                            _isPregnant,
+                                            _profilePicFile,
+                                            _months,
+                                            _birthDateBaby,
+                                            context
+                                        );
+                                        setState(() {
+                                          _isSaved = true;
+                                        });
+                                        showSnackBar(context, "Başarılı");
+                                        Navigator.pop(context);
+                                      }else{
+                                        showSnackBar(context, "Bir şeyler ters gitti");
+                                      }
+                                    }on Exception catch (_, ex){
+                                      print("RECO : $ex");
                                     }
                                   },
                                   child: const Text("Kaydet")),

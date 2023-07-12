e4a8acdcc35911454135c67a4122b2ef4a567b97
Umut <umtakpnr5@gmail.com>
2023-07-13 00:19:21 +0300

    Kullanıcı şifresini güncelleyebilir.

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,5 @@
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:form_validator/form_validator.dart';
 
@@ -22,4 +23,33 @@ extension CustomValidationBuilderRepeatPassword on ValidationBuilder {
         }
         return null;
       });
+}
+
+//ValidationBuilder classına şifreleri karşılaştırma eklentisi
+extension CustomValidationBuilderCheckOldPass on ValidationBuilder {
+  checkOldPassword(TextEditingController passwdOld, TextEditingController passwdNew) => add((value) {
+    if (passwdOld.text == passwdNew.text) {
+      return 'Yeni şifreniz eskisiyle aynı olamaz!';
+    }
+    return null;
+  });
+}
+
+String? findAuthExceptionErrorMessage(FirebaseAuthException error){
+  String? message;
+  switch(error.code){
+    case 'too-many-requests':
+      message = "Bu cihazdan çok fazla istek aldık. Lütfen daha sonra tekrar deneyin ya da şifrenizi sıfırlayın.";
+      break;
+    case 'wrong-password':
+      message = "Girdiğiniz şifre hatalı. Lütfen tekrar deneyin ya da şifrenizi sıfırlayın.";
+      break;
+    case 'user-disabled':
+      message = "Bu hesap silinmiş ya da banlanmış.";
+      break;
+    default :
+      message = "Şifre güncellenirken bir hata oluştur. Şifrenizi sıfırlamayı deneyin.";
+      break;
+  }
+  return message;
 }
--- lib/features/profile/screens/change_password_screen.dart
+++ lib/features/profile/screens/change_password_screen.dart
@@ -1,4 +1,6 @@
 import 'package:f21_demo/core/utils.dart';
+import 'package:f21_demo/features/auth/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,16 +17,24 @@ class ChangePasswordScreen extends ConsumerStatefulWidget {
 }
 
 class _ChangePasswordScreenState extends ConsumerState<ChangePasswordScreen> {
+  late AuthRepository authProvider;
   final _formKey = GlobalKey<FormBuilderState>();
   final _passwordController1 = TextEditingController();
   final _passwordController2 = TextEditingController();
+  final _passwordControllerOld = TextEditingController();
+
+
   //For Show/Hide Password Button
   bool isShowPassword = true;
-  void _toggleShowPassword(){
+
+  void _toggleShowPassword() {
     setState(() {
       isShowPassword = !isShowPassword;
     });
   }
+
+  //isLoading
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -40,94 +50,157 @@ class _ChangePasswordScreenState extends ConsumerState<ChangePasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, viewportConstraints) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Şifreni Değiştir"),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            constraints: BoxConstraints(
-              maxHeight: viewportConstraints.maxHeight,
-              maxWidth: viewportConstraints.maxHeight,
-            ),
-            child: IntrinsicHeight(
-              child: Padding(
-                padding: const EdgeInsets.all(14),
-                child: FormBuilder(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      FormBuilderTextField(
-                        validator: ValidationBuilder(localeName: 'tr')
-                            .required()
-                            .minLength(6)
-                            .repeatPassword(_passwordController1, _passwordController2)
-                            .build(),
-                        name: "password_1",
-                        obscureText: isShowPassword,
-                        controller: _passwordController1,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              _toggleShowPassword();
-                            },
-                            icon: isShowPassword ? const Icon(Icons.visibility_off)  : const Icon(Icons.visibility),
+    final User user = ref.read(authRepositoryProvider).getCurrentUser()!;
+    return LayoutBuilder(
+      builder: (context, viewportConstraints) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Şifreni Değiştir"),
+          ),
+          body: _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : SingleChildScrollView(
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxHeight: viewportConstraints.maxHeight,
+                      maxWidth: viewportConstraints.maxHeight,
+                    ),
+                    child: IntrinsicHeight(
+                      child: Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: FormBuilder(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              FormBuilderTextField(
+                                validator: ValidationBuilder(localeName: 'tr')
+                                    .required()
+                                    .minLength(6)
+                                    .checkOldPassword(_passwordControllerOld,
+                                        _passwordController2)
+                                    .build(),
+                                name: "password_old",
+                                obscureText: isShowPassword,
+                                controller: _passwordControllerOld,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      _toggleShowPassword();
+                                    },
+                                    icon: isShowPassword
+                                        ? const Icon(Icons.visibility_off)
+                                        : const Icon(Icons.visibility),
+                                  ),
+                                  label: const Text("Eski Şifreniz"),
+                                  hintText: "Eski şifrenizi girin.",
+                                  contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                    horizontal: 15,
+                                  ),
+                                  fillColor: CustomStyles.fillColor,
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              FormBuilderTextField(
+                                validator: ValidationBuilder(localeName: 'tr')
+                                    .required()
+                                    .minLength(6)
+                                    .repeatPassword(_passwordController1,
+                                        _passwordController2)
+                                    .build(),
+                                name: "password_1",
+                                obscureText: isShowPassword,
+                                controller: _passwordController1,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      _toggleShowPassword();
+                                    },
+                                    icon: isShowPassword
+                                        ? const Icon(Icons.visibility_off)
+                                        : const Icon(Icons.visibility),
+                                  ),
+                                  label: const Text("Yeni Şifreniz"),
+                                  hintText: "Yeni şifrenizi girin.",
+                                  contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                    horizontal: 15,
+                                  ),
+                                  fillColor: CustomStyles.fillColor,
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              FormBuilderTextField(
+                                validator: ValidationBuilder(localeName: 'tr')
+                                    .required()
+                                    .minLength(6)
+                                    .repeatPassword(_passwordController1,
+                                        _passwordController2)
+                                    .build(),
+                                name: "password_2",
+                                obscureText: isShowPassword,
+                                controller: _passwordController2,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      _toggleShowPassword();
+                                    },
+                                    icon: isShowPassword
+                                        ? const Icon(Icons.visibility_off)
+                                        : const Icon(Icons.visibility),
+                                  ),
+                                  label: const Text("Şifre Tekrar"),
+                                  hintText: "Yeni şifrenizi tekrar girin",
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 15),
+                                  fillColor: CustomStyles.fillColor,
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: () async {
+                                  setState(() {
+                                    _isLoading = true;
+                                  });
+                                  if (_formKey.currentState!.validate()) {
+                                    var newPassword = _passwordController2.text;
+                                    var oldPassword = _passwordControllerOld.text;
+
+                                    final credential = EmailAuthProvider.credential(
+                                        email: user.email!, password: oldPassword);
+
+                                    Future<bool> isLoading;
+
+                                    await user.reauthenticateWithCredential(credential).then((value) async {
+                                      await user.updatePassword(newPassword).then((_) {
+                                        //Değiştirme işlemi başarılı
+                                        showSnackBar(context, "Şifre başarıyla değiştirildi.");
+                                        context.pop();
+                                      }).catchError((error) {
+                                        showSnackBar(context, findAuthExceptionErrorMessage(error)!);
+                                      });
+                                    }).catchError((error) {
+                                      showSnackBar(context, findAuthExceptionErrorMessage(error)!);
+                                    }).then((value) {
+                                        setState(() {
+                                          _isLoading = false;
+                                        });
+                                    },);
+
+                                  }
+                                },
+                                child: const Text("Şifremi Değiştir"),
+                              ),
+                            ],
                           ),
-                          label: const Text("Yeni Şifreniz"),
-                          hintText: "Yeni şifrenizi girin.",
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 15,
-                              horizontal: 15,
-                          ),
-                          fillColor: CustomStyles.fillColor,
                         ),
                       ),
-                      const SizedBox(height: 20),
-                      FormBuilderTextField(
-                        validator: ValidationBuilder(localeName: 'tr')
-                          .required()
-                          .minLength(6)
-                          .repeatPassword(_passwordController1, _passwordController2)
-                          .build(),
-                        name: "password_2",
-                        obscureText: isShowPassword,
-                        controller: _passwordController2,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              _toggleShowPassword();
-                            },
-                            icon: isShowPassword ? const Icon(Icons.visibility_off)  : const Icon(Icons.visibility),
-                          ),
-                          label: const Text("Şifre Tekrar"),
-                          hintText: "Yeni şifrenizi tekrar girin",
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 15),
-                          fillColor: CustomStyles.fillColor,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                          onPressed: () {
-                             if(_formKey.currentState!.validate()){
-                               //TODO: Şifre değiştirme işlemi yapılmalı (firebase ile).
-                               //TODO: İşlem başarılıysa snackbar göster.
-                               showSnackBar(context, "Şifreniz başarıyla değiştirildi ...");
-                               context.pop(); //Önceki context'e dön
-                             }
-                          },
-                          child: const Text("Şifremi Değiştir"),
-                      )
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
-        ),
-      );
-    },
+        );
+      },
     );
   }
 }
+

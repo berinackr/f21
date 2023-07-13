85faa660399401df43da1eb32a9cd3caebbe772f
Umut <umtakpnr5@gmail.com>
2023-07-13 23:47:14 +0300

    Profil ekranındaki 18 yaş sorunu fixlendi

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -348,7 +348,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                             return null;
                           },
                           name: "birthDate",
-                          lastDate: DateTime(DateTime.now().year - 18),
+                          //lastDate: DateTime(DateTime.now().year - 18),
                           initialDate: _birthDate,
                           initialValue: _birthDate,
                           onChanged: (value) {
@@ -446,7 +446,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                             _birthDateBaby ?? DateTime.now(),
                             initialDate:
                             _birthDateBaby ?? DateTime.now(),
-                            name: "birthDate",
+                            name: "birthDateBaby",
                             onChanged: (valueBaby) {
                               _birthDateBaby = valueBaby;
                             },
